9ae5ea316bd3a51f5a3c40cd6a5886884b49f535
djniche <71032074+djniche@users.noreply.github.com>
2024-09-13 01:34:26 +0800

    Update stripchat.py

--- streamonitor/sites/stripchat.py
+++ streamonitor/sites/stripchat.py
@@ -11,7 +11,7 @@ class StripChat(Bot):
         self.vr = False
 
     def getWebsiteURL(self):
-        return "https://stripchat.com/" + self.username
+        return "https://stripchat.com/" + self.用户名
 
     def getVideoUrl(self):
         return self.getWantedResolutionPlaylist(None)
@@ -19,7 +19,6 @@ class StripChat(Bot):
     def getPlaylistVariants(self, url):
         def formatUrl(master, auto):
             return "https://edge-hls.{host}/hls/{id}{vr}/{master}/{id}{vr}{auto}.m3u8".format(
-            server=self.lastInfo["cam"]["viewServers"]["flashphoner-hls"],
             host='doppiocdn.com',
             id=self.lastInfo["cam"]["streamName"],
             master='master' if master else '',
@@ -34,20 +33,20 @@ class StripChat(Bot):
         return variants
 
     def getStatus(self):
-        r = requests.get('https://stripchat.com/api/vr/v2/models/username/' + self.username, headers=self.headers)
+        r = requests.get('https://stripchat.com/api/vr/v2/models/username/' + self.用户名, headers=self.headers)
         if r.status_code != 200:
-            return Bot.Status.UNKNOWN
+            return Bot.状态.UNKNOWN
 
         self.lastInfo = r.json()
 
         if self.lastInfo["model"]["status"] == "public" and self.lastInfo["isCamAvailable"] and self.lastInfo['cam']["isCamActive"]:
-            return Bot.Status.PUBLIC
+            return Bot.状态.公共
         if self.lastInfo["model"]["status"] in ["private", "groupShow", "p2p", "virtualPrivate", "p2pVoice"]:
-            return Bot.Status.PRIVATE
+            return Bot.状态.私有
         if self.lastInfo["model"]["status"] in ["off", "idle"]:
-            return Bot.Status.OFFLINE
+            return Bot.状态.OFFLINE
         self.logger.warn(f'Got unknown status: {self.lastInfo["model"]["status"]}')
-        return Bot.Status.UNKNOWN
+        return Bot.状态.UNKNOWN
 
 
-Bot.loaded_sites.add(StripChat)
+Bot.loaded_sites.添加(StripChat)
